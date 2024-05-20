a74f1c15943151938234733c685c619726cfcbc9
bishal1998 <bishalpaudel1998@gmail.com>
2024-05-20 07:45:34 +0545

    accordion done

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import { dataItems } from "./data";
+
 const App = () => {
-  return <div>App</div>;
+  const [selected, setSelected] = useState("section1");
+
+  const handleSelected = (id) => {
+    setSelected(selected === id ? null : id);
+  };
+  return (
+    <div>
+      {dataItems.map((item) => (
+        <div key={item.id}>
+          <div onClick={() => handleSelected(item.id)}>{item.title}</div>
+          {selected === item.id && <div>{item.content}</div>}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
---
+++ accordion/src/data.js
@@ -0,0 +1,20 @@
+export const dataItems = [
+  {
+    id: "section1",
+    title: "Section 1: Introduction",
+    content:
+      "Welcome to the introduction section. This part provides an overview of the topics covered in this document. You'll find a brief summary and important highlights to get you started.",
+  },
+  {
+    id: "section2",
+    title: "Section 2: Details",
+    content:
+      "This section delves into the details of the subject matter. Here, you'll find in-depth explanations, statistics, and relevant examples to help you understand the concepts thoroughly.",
+  },
+  {
+    id: "section3",
+    title: "Section 3: Conclusion",
+    content:
+      "In the conclusion section, we summarize the key points discussed and provide final thoughts. This part will help you review the information and understand the overall message.",
+  },
+];
